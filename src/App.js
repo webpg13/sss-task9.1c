3030507fddc9689918edba7e4503009b53ca7ace
“cmh” <874220809@qq.com>
2022-09-06 14:12:28 +0800

    build

--- src/App.js
+++ src/App.js
@@ -5,13 +5,13 @@ import Login from './views/Login'
 import Register from './views/Register'
 import Home from './views/Home'
 // 路由 react-router-dom@5.2.1
-import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom'
+import { BrowserRouter, HashRouter, Route, Switch, Redirect } from 'react-router-dom'
 
 // BrowserRouter 只能出现一次
 class App extends React.Component {
   render() {
     return <div>
-      <BrowserRouter>
+      <HashRouter>
         <Switch>
           <Route exact path="/login" component={Login} ></Route>
           <Route exact path="/register" component={Register} ></Route>
@@ -19,7 +19,7 @@ class App extends React.Component {
           {/* 重定向 */}
           <Redirect exact from="/" to="login"></Redirect>
         </Switch>
-      </BrowserRouter>
+      </HashRouter>
     </div>
   }
 }
